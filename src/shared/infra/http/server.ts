862a0e9f33aab4e2369dc5fa1cffbbdaaf95cf63
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-05-20 01:37:51 -0300

    sets repositories DI & routes controllers

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,12 +3,13 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import uploadConfig from './config/upload';
 
-import AppError from './errors/AppError';
-
-import './database';
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
@@ -25,7 +26,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
-  console.error(err);
+  console.log(err);
 
   return response.status(500).json({
     status: 'error',

ecae4ba99ac7b3818b33562c1f3436b0ae6a9aaf
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-05-21 00:00:17 -0300

    adds new tests

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,28 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+import CreateUserService from './CreateUserService';
+import AuthenticateUserService from './AuthenticateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    const createUser = new CreateUserService(fakeUsersRepository);
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+  });
+});
